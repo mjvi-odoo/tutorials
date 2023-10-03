3e0fe586351f6fdcb1520f2868dceee9fc19b161
mjvi-odoo <mjvi@odoo.com>
2023-10-03 18:29:48 +0530

    add ex-1 and ex-2 of ch-2

--- owl_playground/static/src/todo/todo.js
+++ owl_playground/static/src/todo/todo.js
@@ -22,15 +22,6 @@ export class Todo extends Component {
         });
     }
     
-
-
     static template = "owl_playground.todo";
 }
-// Todo.props = {
-//     id: {type:Number},
-//     description: {type:String},
-//     done: {type:Boolean},
-//     toggleState: { type:Function },
-//     removeTodo: { type:Function },  
-// };
 
